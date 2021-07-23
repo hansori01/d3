8cc9759c6994a71c2e5c4b816584e668a490822b
Sori Han <19sorihan88@gmail.com>
2021-07-22 21:58:42 -0700

    'finish'

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,13 +1,18 @@
-d3.selectAll(".day-high .temp")
-  .data([45, 78, 77, 66, 76])
-  .html(function(d, i) {
-    if(i==0) {
-      return '<strong class="text-muted" style="font-size: 2rem">' + d + '</strong>';
-    } else {
-      if (d >= 77) {
-        return '<strong class="text-danger">' + d + '</strong>';
-      } else {
-        return '<strong>' + d + '</strong>';
-      }
-    }
-  })
+const myData = [
+  { date: '4/01/2020', low: 23, high: 80 },
+  { date: '4/01/2020', low: 22, high: 66 },
+  { date: '4/01/2020', low: 44, high: 59 },
+  { date: '4/01/2020', low: 55, high: 98 },
+  { date: '4/01/2020', low: 33, high: 81 }
+];
+
+d3.select('tbody')
+  .selectAll('tr') // this 'tr' does not exist yet
+  .data(myData)
+  .enter() //access the sublevel of the data
+  .append('tr')
+  .html(function (d) {
+    return `<th scope='row'> ${d.date} </th>
+    <td> ${d.low}</td>
+    <td> ${d.high}</td>`;
+  });
