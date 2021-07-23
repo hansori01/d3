0e6a18b75376e03b20d4de6f738c33b6245fb853
Sori Han <19sorihan88@gmail.com>
2021-07-22 21:33:18 -0700

    attributes and selecting

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,7 +1,22 @@
-d3.selectAll("tr:nth-child(1) .day-high .temp")
-  .html("<strong>hot</strong>")
-  .insert('span', 'strong')
-  .html('today ');
+d3.selectAll("tr:nth-child(4) .day-high")
+  .append('span')
+  .html('HOT')
+  // .style('background', 'red')
+  // .style('color', 'white')
+  // .style('font-weight', 'bold')
+  // .style('padding', '5px')
+  // .style('border-radius', '5px')
+  // .style('margin-left', '5px')
+  .classed('label label-danger', true)
+  .classed('label-danger', false)
+  .classed('label-warning', true)
+  .style('margin-left', "3px")
 
-  d3.selectAll('tr:nth-child(3)')
-    .remove();
+
+d3.select('h2 span')
+  .classed('small', false)
+
+d3.selectAll('tr')
+  .insert('td', ':first-child') //injects the new element before the first child
+  .append('input') //appends the new element to the selected elements
+  .attr('type', 'checkbox')
