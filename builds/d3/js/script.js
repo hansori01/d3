18251cc388195c6467879044cb9a725ef5eef5b7
Sori Han <19sorihan88@gmail.com>
2021-07-23 16:36:04 -0700

    scale time

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -3,12 +3,15 @@ d3.json('js/data/forecast.json', function(d) {
   var temperatures = [],
       margin = { top: 0, right: 0, bottom: 30, left: 20 }
       height = 400 - margin.top - margin.bottom,
-      width = 600 - margin.left - margin.right;
+      width = 600 - margin.left - margin.right,
+      dates= []
 
   var   tempColor,
         yScale,
         yAxisValues,
         yAxisTicks,
+        xAxisValues,
+        xAxisTicks,
         xScale,
         colors,
         tooltip,
@@ -16,6 +19,7 @@ d3.json('js/data/forecast.json', function(d) {
 
   for (var i = 0; i<d.list.length; i++) {
     temperatures.push(d.list[i].main.temp);
+    dates.push( new Date(d.list[i].dt_txt));
   }
 
   yScale = d3.scaleLinear()
@@ -34,6 +38,13 @@ d3.json('js/data/forecast.json', function(d) {
     .paddingInner(.1)
     .paddingOuter(.1)
     .range([0, width])
+
+  xAxisValues = d3.scaleTime()
+    .domain([dates[0], dates[dates.length-1]])
+    .range([0, width])  
+
+  xAxisTicks = d3.axisBottom(xAxisValues)
+  .ticks(d3.timeDay.every(1))
 
   colors = d3.scaleLinear()
     .domain([0, 65, d3.max(temperatures)])
@@ -87,6 +98,11 @@ d3.json('js/data/forecast.json', function(d) {
   yGuide = d3.select('#viz svg').append('g')
             .attr('transform', 'translate(20,0)')
             .call(yAxisTicks)
+
+  xGuide = d3.select('#viz svg').append('g')
+            .attr('transform', 'translate(20, ' + height + ')')
+            .call(xAxisTicks)
+
 
   myChart.transition()
     .attr('height', function(d) {
