de8e56e7f29f55f23f5f306ecbe6b2890a719daf
Sori Han <19sorihan88@gmail.com>
2021-07-22 21:47:23 -0700

    'finish'

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,23 +1,9 @@
-d3.selectAll("tr:nth-child(4) .day-high")
-  .append('span')
-  .html('hot')
-
-  // .style('background', 'red')
-  // .style('padding', '3px')
-  // .style('margin-left', '3px')
-  // .style('border-radius', '3px')
-  // .style('color', 'white')
-
-  .classed('label label-danger', true)
-  .classed('label-danger', false)
-  .classed('label-warning', true)
-  .style('margin-left', '3px')
-
-d3.select('h2 span')
-  .classed('small', false)
-
-d3.selectAll('tr')
-  .insert('td', ':first-child')
-  .append('input')
-  .attr('type', 'checkbox')
-  .property('checked', true)
+d3.selectAll('.day-high .temp')
+  .data([45, 44, 54])
+  .html(function (d, i) {
+    //d is iterated element in the data array
+    if (i === 0) {
+      return d;
+    }
+    return `<strong>${d}</strong>`;
+  });
